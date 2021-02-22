ec94943dc583324bde10823fe3633bfa08dcb424
Reinaldo Neto <reinaldonetof@hotmail.com>
2021-02-21 23:43:04 -0300

    membros da classe

--- src/main/kotlin/one.digitalinovation.digionebank/Pessoa.kt
+++ src/main/kotlin/one.digitalinovation.digionebank/Pessoa.kt
@@ -2,14 +2,16 @@ package one.digitalinovation.digionebank
 
 class Pessoa {
     var nome: String = "Reinaldo"
-
     var cpf: String = "051.514.993-43"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main() {
     val reinaldo = Pessoa()
 
-    println(reinaldo.nome)
-    println(reinaldo.cpf)
+    println(reinaldo.pessoaInfo())
 }
