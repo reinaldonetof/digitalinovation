191aa11601488ffc0daceef6f6ae9d0648e05aa5
Reinaldo Neto <reinaldonetof@hotmail.com>
2021-03-06 00:59:26 -0300

    Gerente.kt, Analista.kt subclasses

---
+++ src/main/kotlin/one.digitalinovation.digionebank/Gerente.kt
@@ -0,0 +1,5 @@
+package one.digitalinovation.digionebank
+
+class Gerente(nome: String, cpf: String, salario: Double) : Funcionario(nome, cpf, salario) {
+    override fun calculoAuxilio(): Double = salario * 0.4
+}
